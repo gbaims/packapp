d12e2bed987c28a5c6eda231185009393fe4663a
Gabriel Baims <gabriel.a.baims@gmail.com>
2023-06-17 13:56:14 -0300

    create first web server

--- gbaims/packapp/__init__.py
+++ gbaims/packapp/__init__.py
@@ -1,1 +1,11 @@
-print('hello!')
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.get("/")
+    def root():
+        return {"hello": "world"}
+
+    return app
